40ea1034bf02197bcd6707515cbf61305f37d39a
Shreyansh Saurabh <shreyansh.saurabh01@gmail.com>
2025-11-16 03:24:47 -0500

    update db conn

--- db/database.py
+++ db/database.py
@@ -6,6 +6,6 @@ from utils.configs import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,pool_pre_ping=True, pool_recycle=1800,pool_size=5,max_overflow=10)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
